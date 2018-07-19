76224491d6add148da0719d4190b5e8651e04c44
Syldexia <syldexia@ofthewi.red>
2018-07-19 22:11:40 +0100

    Generate tests with macros (I'm sure this won't go wrong in the future)

--- tests/vm/binary.c
+++ tests/vm/binary.c
@@ -1,6 +1,27 @@
 #include "vm/soft-vm.h"
 #include "vm/vm-test.h"
 
+void soft_vm_test_and_int_reg(void **state)
+{
+	union SoftData bin_1, bin_2;
+	int res;
+	bin_1.soft_int32 = 0b00111100;
+	bin_2.soft_int32 = 0b00001101;
+	res              = 0b00001100;
+	soft_instr test_program[] = {
+		// opcode,         datatype,      src,   dest, immediate
+		{ soft_instr_load, soft_int32_t,  noop,  0x1,  {bin_1.soft_int32} },
+		{ soft_instr_load, soft_int32_t,  noop,  0x2,  {bin_2.soft_int32} },
+		{ soft_instr_and,  soft_int32_t,   0x1,  0x2,  {noop} },
+		{ halt,            noop,          noop,  noop, {noop} }
+	};
+
+	soft_vm_load_program(&vm, test_program);
+	soft_vm_run_vm(&vm);
+
+	assert_true(res == vm.r[0x2].soft_int32);
+}
+
 void soft_vm_test_and_float_reg(void **state)
 {
 	union SoftData bin_1, bin_2;
--- tests/vm/vm-test.h
+++ tests/vm/vm-test.h
@@ -4,6 +4,15 @@
 #include "soft-test.h"
 #include "vm/soft-vm.h"
 
+#define _SOFT_VM_TEST_LOAD_AND_RUN(vm, program) \
+	soft_vm_load_program(&vm, test_program); \
+	soft_vm_run_vm(&vm);
+
+#define _SOFT_VM_TEST_LOAD_AND_RUN_DEBUG(vm, program) \
+	soft_vm_load_program(&vm, test_program); \
+	soft_vm_run_vm_debug(&vm); \
+	printf("\n");
+
 soft_VM vm;
 
 /**
@@ -16,10 +25,25 @@ void soft_vm_test_load_float(void **state);
  * Arithmetic instruction tests
  * */
 void soft_vm_test_add_int32_reg(void **state);
+void soft_vm_test_sub_int32_reg(void **state);
+void soft_vm_test_mul_int32_reg(void **state);
+void soft_vm_test_div_int32_reg(void **state);
+
 void soft_vm_test_add_int32_imm(void **state);
+void soft_vm_test_sub_int32_imm(void **state);
+void soft_vm_test_mul_int32_imm(void **state);
+void soft_vm_test_div_int32_imm(void **state);
 
 void soft_vm_test_add_float_reg(void **state);
+void soft_vm_test_sub_float_reg(void **state);
+void soft_vm_test_mul_float_reg(void **state);
+void soft_vm_test_div_float_reg(void **state);
+
 void soft_vm_test_add_float_imm(void **state);
+void soft_vm_test_sub_float_imm(void **state);
+void soft_vm_test_mul_float_imm(void **state);
+void soft_vm_test_div_float_imm(void **state);
+
 
 /**
  * Comparison instruction tests
