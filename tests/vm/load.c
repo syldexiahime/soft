e51a9d7538dc950e30deeadc6ab446f51fb733c0
Sophie Askew <sophie@ofthewi.red>
2018-07-22 13:50:24 +0100

    Wrote more tests

--- tests/vm/load.c
+++ tests/vm/load.c
@@ -1,34 +1,21 @@
 #include "vm/soft-vm.h"
 #include "vm/vm-test.h"
 
-void soft_vm_test_load_int32(void **state)
-{
-	union SoftData test_data;
-	test_data.soft_int32 = 12345;
-	soft_instr test_program[] = {
-		// opcode,         datatype,      src,   dest, immediate
-		{ soft_instr_load, soft_int32_t,  noop,  0x1,  {test_data.soft_int32} },
-		{ halt,            noop,          noop,  noop, {noop} }
-	};
-
-	soft_vm_load_program(&vm, test_program);
-	soft_vm_run_vm(&vm);
-
-	assert_true(test_data.soft_int32 == vm.r[0x1].soft_int32);
+#define SOFT_VM_LOAD_TEST(type, value) \
+void soft_vm_test_load_##type(void **state) \
+{ \
+	union SoftData test_data; \
+	test_data.soft_##type = value; \
+	soft_instr test_program[] = { \
+		{ soft_instr_load, soft_int32_t,  noop,  0x1,  {test_data.soft_int32} }, \
+		{ halt,            noop,          noop,  noop, {noop} } \
+	}; \
+\
+	soft_vm_load_program(&vm, test_program); \
+	soft_vm_run_vm(&vm); \
+\
+	assert_true(test_data.soft_##type == vm.r[0x1].soft_##type); \
 }
 
-void soft_vm_test_load_float(void **state)
-{
-	union SoftData test_data;
-	test_data.soft_float = 0x4293f5c3;
-	soft_instr test_program[] = {
-		// opcode,         datatype,      src,   dest, immediate
-		{ soft_instr_load, soft_float_t,  noop,  0x1,  {test_data.soft_int32} },
-		{ halt,            noop,          noop,  noop, {noop} }
-	};
-
-	soft_vm_load_program(&vm, test_program);
-	soft_vm_run_vm(&vm);
-
-	assert_true(test_data.soft_float == vm.r[0x1].soft_float);
-}
+SOFT_VM_LOAD_TEST(int32, 12345)
+SOFT_VM_LOAD_TEST(float, 12.45)
