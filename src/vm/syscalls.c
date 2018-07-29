5efa9760438f1183207a70fb4109f8cb83593e41
Sophie Askew <sophie@ofthewi.red>
2018-07-29 15:48:43 +0100

    Split up vm files a bit

--- src/vm/syscalls.c
+++ src/vm/syscalls.c
@@ -15,7 +15,7 @@ void soft_vm_syscall(soft_VM *vm, soft_instr instr)
 	switch (instr.imm.soft_int32) {
 
 		case soft_sys_open: {
-			char*  buffer = (char*)  vm->r[0].soft_ptr32;
+			char*  buffer = (char*)  vm->r[0].soft_ptr;
 			int    oflags =          vm->r[1].soft_int32;
 			mode_t mode   = (mode_t) vm->r[2].soft_int32;
 
@@ -25,7 +25,7 @@ void soft_vm_syscall(soft_VM *vm, soft_instr instr)
 
 		case soft_sys_read: {
 			int    fildes =          vm->r[0].soft_int32;
-			void*  buffer =          vm->r[1].soft_ptr32;
+			void*  buffer =          vm->r[1].soft_ptr;
 			size_t nbytes = (size_t) vm->r[2].soft_int32;
 
 			vm->r[instr.dst].soft_int32 = read(fildes, buffer, nbytes);
@@ -34,7 +34,7 @@ void soft_vm_syscall(soft_VM *vm, soft_instr instr)
 
 		case soft_sys_write: {
 			int    fildes =          vm->r[0].soft_int32;
-			void*  buffer =          vm->r[1].soft_ptr32;
+			void*  buffer =          vm->r[1].soft_ptr;
 			size_t nbytes = (size_t) vm->r[2].soft_int32;
 
 			vm->r[instr.dst].soft_int32 = write(fildes, buffer, nbytes);
@@ -44,12 +44,12 @@ void soft_vm_syscall(soft_VM *vm, soft_instr instr)
 		case soft_sys_malloc: {
 			size_t nbytes = (size_t) vm->r[0].soft_int32;
 
-			vm->r[instr.dst].soft_ptr32 = malloc(nbytes);
+			vm->r[instr.dst].soft_ptr = malloc(nbytes);
 			break;
 		}
 
 		case soft_sys_free: {
-			void* ptr = vm->r[0].soft_ptr32;
+			void* ptr = vm->r[0].soft_ptr;
 
 			free(ptr);
 			break;
