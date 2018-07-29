053fa9add03eade9562b6aae54999563f09fa6fa
Sophie Askew <sophie@ofthewi.red>
2018-07-29 15:48:43 +0100

    Split up vm files a bit

--- src/vm/soft-vm.h
+++ src/vm/soft-vm.h
@@ -1,37 +1,12 @@
 #ifndef _SOFT_VM_H
 #define _SOFT_VM_H
 
-#include <stdio.h>
-#include <stdint.h>
-
-#include "vm/opcodes.h"
 #include "soft.h"
+#include "vm/opcodes.h"
+#include "vm/instructions.h"
+#include "vm/datatypes.h"
 
 #define SOFT_VM_NUM_REGS 8
-
-typedef enum {
-	soft_ptr32 = 0x00,
-	soft_int32 = 0x10,
-	soft_float = 0x20,
-} soft_DATATYPES;
-
-union SoftData {
-	int32_t   soft_int32;
-	float     soft_float;
-	void*     soft_ptr32;
-};
-
-#define soft_int32_t int32_t
-#define soft_float_t float
-#define soft_ptr32_t void*
-
-typedef struct {
-	uint16_t opcode : 8;
-	uint16_t datatype : 8;
-	uint8_t src : 4;
-	uint8_t dst : 4;
-	union SoftData imm;
-} soft_instr;
 
 typedef struct {
 	boolean running;
