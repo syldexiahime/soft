d0d4562773e7e6c8ac9e8869c93817ee754999a2
Sophie Askew <sophie@ofthewi.red>
2018-08-26 20:57:53 +0100

    Updated the way charstream works to be more procedural

--- src/utils/charstream.h
+++ src/utils/charstream.h
@@ -4,18 +4,20 @@
 #include <stdbool.h>
 
 typedef struct {
-	char* buffer;
-	int index;
-	int line;
-	int column;
+	int    index;
+	int    line;
+	int    column;
+	char * buffer;
 } soft_charstream;
 
-void soft_charstream_init(soft_charstream *stream, char *buffer);
-char soft_charstream_peek(soft_charstream *stream);
-char soft_charstream_consume(soft_charstream *stream);
-void soft_charstream_skip(soft_charstream *stream);
-bool soft_charstream_eof(soft_charstream *stream);
-bool soft_charstream_expect(soft_charstream *stream, bool (*eval_function)(char));
-char* soft_charstream_read_while(soft_charstream * charstream, bool (*eval_function)(char));
+soft_charstream * soft_charstream_init(char * buffer);
+char soft_charstream_peek();
+char soft_charstream_consume();
+void soft_charstream_skip();
+bool soft_charstream_eof();
+bool soft_charstream_expect(bool (*eval_function)(char));
+char * soft_charstream_read_while(bool (*eval_function)(char));
+char * soft_charstream_read_whilei(bool (*eval_function)(char, int));
+char * soft_charstream_warn(char * filename, char * message);
 
 #endif // _SOFT_CHARSTREAM_H
