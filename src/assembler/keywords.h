7ea8ee5e199bf33895861fab413485d591caa1e2
Sophie Askew <sophie@ofthewi.red>
2018-09-23 18:14:23 +0100

    started assembler stuff

---
+++ src/assembler/keywords.h
@@ -0,0 +1,15 @@
+#ifndef _SOFT_ASM_KEYWORDS_H
+#define _SOFT_ASM_KEYWORDS_H
+
+#include <stdlib.h>
+
+extern enum SOFT_ASM_KEYWORDS SOFT_ASM_KEYWORDS;
+extern enum SOFT_ASM_REGISTERS SOFT_ASM_REGISTERS;
+extern char * soft_asm_symbols[];
+extern size_t num_soft_asm_symbols;
+extern char * soft_asm_keywords[];
+extern size_t num_soft_asm_keywords;
+extern char * soft_asm_registers[];
+extern size_t num_soft_asm_registers;
+
+#endif // _SOFT_ASM_KEYWORDS_H
