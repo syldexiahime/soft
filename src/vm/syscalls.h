866e3d47c90eb5412cbc8d8cbffa24908c3eab4f
Syldexia <syldexia@ofthewi.red>
2018-09-23 17:15:16 +0100

    cleaned up code a bit

--- src/vm/syscalls.h
+++ src/vm/syscalls.h
@@ -1,11 +1,26 @@
+#ifndef _SOFT_VM_SYSCALLS_H
+#define _SOFT_VM_SYSCALLS_H
+
 #include "vm/soft-vm.h"
 
-enum {
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+ 
+/* Not technically required, but needed on some UNIX distributions */
+#include <sys/types.h>
+#include <sys/stat.h>
+
+enum soft_syscalls {
 	soft_sys_open,
 	soft_sys_read,
 	soft_sys_write,
 	soft_sys_malloc,
 	soft_sys_free
-} _SOFT_SYSCALLS;
+};
 
-void soft_vm_syscall(soft_VM *vm, soft_instr instr);
+struct soft_vm;
+
+void soft_vm_syscall(struct soft_vm * vm, struct soft_instr instr);
+
+#endif // _SOFT_VM_SYSCALLS_H
