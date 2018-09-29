2c6aa73bedc180c7b4f18ef6a7f8feca83833e13
Sophie Askew <sophie@ofthewi.red>
2018-09-29 15:11:58 +0100

    WIP: use jump table for vm

--- src/vm/soft-vm.c
+++ src/vm/soft-vm.c
@@ -19,46 +19,105 @@ void soft_vm_load_program(struct soft_vm * vm, struct soft_instr * program)
 	soft_vm_init_vm(vm);
 }
 
+#define make_jump_table(soft_type_t) \
+{ \
+	&&halt, \
+	&&noop, \
+	&&syscall, \
+	&&soft_type_t##_load, \
+	&&soft_type_t##_movreg, \
+	&&soft_type_t##_movaddr, \
+	&&soft_type_t##_add, \
+	&&soft_type_t##_sub, \
+	&&soft_type_t##_mul, \
+	&&soft_type_t##_div, \
+	&&soft_type_t##_eq, \
+	&&soft_type_t##_gt, \
+	&&soft_type_t##_lt, \
+	&&soft_type_t##_gteq, \
+	&&soft_type_t##_lteq, \
+	&&soft_type_t##_and, \
+	&&soft_type_t##_or, \
+	&&soft_type_t##_xor, \
+	&&soft_type_t##_lshift, \
+	&&soft_type_t##_rshift, \
+	&&soft_type_t##_not, \
+	&&jmp, \
+	&&jmpz, \
+	&&jmpnz, \
+	&&soft_type_t##_castint32, \
+	&&soft_type_t##_castfloat \
+}
+
+#define make_jump_labels(soft_type_t) \
+	soft_type_t##_load:      soft_vm_load(soft_type_t);           goto end; \
+	soft_type_t##_movreg:    soft_vm_mov_reg(soft_type_t);        goto end; \
+	soft_type_t##_movaddr:   soft_vm_mov_addr(soft_type_t);       goto end; \
+	soft_type_t##_add:       soft_vm_arithmetic(soft_type_t, +);  goto end; \
+	soft_type_t##_sub:       soft_vm_arithmetic(soft_type_t, -);  goto end; \
+	soft_type_t##_mul:       soft_vm_arithmetic(soft_type_t, *);  goto end; \
+	soft_type_t##_div:       soft_vm_arithmetic(soft_type_t, /);  goto end; \
+	soft_type_t##_and:       soft_vm_binary(&);                   goto end; \
+	soft_type_t##_or:        soft_vm_binary(|);                   goto end; \
+	soft_type_t##_xor:       soft_vm_binary(^);                   goto end; \
+	soft_type_t##_lshift:    soft_vm_binary(<<);                  goto end; \
+	soft_type_t##_rshift:    soft_vm_binary(>>);                  goto end; \
+	soft_type_t##_not:       soft_vm_not();                       goto end; \
+	soft_type_t##_eq:        soft_vm_comparison(soft_type_t, ==); goto end; \
+	soft_type_t##_gt:        soft_vm_comparison(soft_type_t, >);  goto end; \
+	soft_type_t##_lt:        soft_vm_comparison(soft_type_t, <);  goto end; \
+	soft_type_t##_gteq:      soft_vm_comparison(soft_type_t, >=); goto end; \
+	soft_type_t##_lteq:      soft_vm_comparison(soft_type_t, <=); goto end; \
+	soft_type_t##_castint32: soft_vm_cast_int32(soft_type_t);     goto end; \
+	soft_type_t##_castfloat: soft_vm_cast_float(soft_type_t);     goto end;
+
 static void soft_vm_decode_instr(struct soft_vm * vm, struct soft_instr instr)
 {
-	switch (instr.opcode) {
-		case halt: vm->running = false; break;
-		case noop: break;
+	static void * jump_table[][num_soft_instrs] = {
+		make_jump_table(soft_ptr),
+		make_jump_table(soft_int32),
+		make_jump_table(soft_float),
+	};
 
-		case soft_instr_syscall:   soft_vm_syscall(vm, instr);                       break;
+	goto * jump_table[instr.datatype][instr.opcode];
 
-		case soft_instr_load:      SOFT_VM_EXECUTE_INSTR(vm, instr, LOAD,);          break;
-		case soft_instr_movreg:    SOFT_VM_EXECUTE_INSTR(vm, instr, MOV_REG,);       break;
-		case soft_instr_movaddr:   SOFT_VM_EXECUTE_INSTR(vm, instr, MOV_ADDR,);      break;
+	invalid:
+	// TODO possibly add some error reporting or something here?
+	goto end;
 
-		case soft_instr_add:       SOFT_VM_EXECUTE_INSTR(vm, instr, ARITHMETIC, +);  break;
-		case soft_instr_sub:       SOFT_VM_EXECUTE_INSTR(vm, instr, ARITHMETIC, -);  break;
-		case soft_instr_mul:       SOFT_VM_EXECUTE_INSTR(vm, instr, ARITHMETIC, *);  break;
-		case soft_instr_div:       SOFT_VM_EXECUTE_INSTR(vm, instr, ARITHMETIC, /);  break;
+	halt:               vm->running = false;              goto end;
+	noop:                                                 goto end;
+	syscall:            soft_vm_syscall(vm, instr);       goto end;
+	jmp:                soft_vm_jmp();                    goto end;
+	jmpz:               soft_vm_jmpz();                   goto end;
+	jmpnz:              soft_vm_jmpnz();                  goto end;
 
-		case soft_instr_eq:        SOFT_VM_EXECUTE_INSTR(vm, instr, COMPARISON, ==); break;
-		case soft_instr_gt:        SOFT_VM_EXECUTE_INSTR(vm, instr, COMPARISON,  >); break;
-		case soft_instr_lt:        SOFT_VM_EXECUTE_INSTR(vm, instr, COMPARISON,  <); break;
-		case soft_instr_gteq:      SOFT_VM_EXECUTE_INSTR(vm, instr, COMPARISON, >=); break;
-		case soft_instr_lteq:      SOFT_VM_EXECUTE_INSTR(vm, instr, COMPARISON, <=); break;
+	soft_ptr_load:      soft_vm_load(soft_ptr);           goto end;
+	soft_ptr_movreg:    soft_vm_mov_reg(soft_ptr);        goto end;
+	soft_ptr_movaddr:   soft_vm_mov_addr(soft_ptr);       goto end;
+	soft_ptr_add:       soft_vm_ptr_arithmetic(+);        goto end;
+	soft_ptr_sub:       soft_vm_ptr_arithmetic(-);        goto end;
+	soft_ptr_eq:        soft_vm_comparison(soft_ptr, ==); goto end;
+	soft_ptr_gt:        soft_vm_comparison(soft_ptr, >);  goto end;
+	soft_ptr_lt:        soft_vm_comparison(soft_ptr, <);  goto end;
+	soft_ptr_gteq:      soft_vm_comparison(soft_ptr, >=); goto end;
+	soft_ptr_lteq:      soft_vm_comparison(soft_ptr, <=); goto end;
+	soft_ptr_castint32: soft_vm_cast_int32(soft_ptr);     goto end;
+	soft_ptr_mul:                                         goto invalid;
+	soft_ptr_div:                                         goto invalid;
+	soft_ptr_and:                                         goto invalid;
+	soft_ptr_or:                                          goto invalid;
+	soft_ptr_xor:                                         goto invalid;
+	soft_ptr_lshift:                                      goto invalid;
+	soft_ptr_rshift:                                      goto invalid;
+	soft_ptr_not:                                         goto invalid;
+	soft_ptr_castfloat:                                   goto invalid;
 
-		case soft_instr_and:       SOFT_VM_EXECUTE_INSTR(vm, instr, BINARY,  &);     break;
-		case soft_instr_or:        SOFT_VM_EXECUTE_INSTR(vm, instr, BINARY,  |);     break;
-		case soft_instr_xor:       SOFT_VM_EXECUTE_INSTR(vm, instr, BINARY,  ^);     break;
-		case soft_instr_lshift:    SOFT_VM_EXECUTE_INSTR(vm, instr, BINARY, <<);     break;
-		case soft_instr_rshift:    SOFT_VM_EXECUTE_INSTR(vm, instr, BINARY, >>);     break;
-		case soft_instr_not:       SOFT_VM_EXECUTE_INSTR(vm, instr, BWNOT,);         break;
+	make_jump_labels(soft_int32);
+	make_jump_labels(soft_float);
 
-		case soft_instr_jmp:       SOFT_VM_EXECUTE_JMP_INSTR(vm, instr, JMP);        break;
-		case soft_instr_jmpz:      SOFT_VM_EXECUTE_JMP_INSTR(vm, instr, JMPZ);       break;
-		case soft_instr_jmpnz:     SOFT_VM_EXECUTE_JMP_INSTR(vm, instr, JMPNZ);      break;
-
-		case soft_instr_castint32: SOFT_VM_EXECUTE_INSTR(vm, instr, CAST_INT32,);    break;
-		case soft_instr_castfloat: SOFT_VM_EXECUTE_INSTR(vm, instr, CAST_FLOAT,);    break;
-
-		default: assert(false); break;
-	}
-	vm->pc++;
+	increment_pc:
+	end: vm->pc++;
 }
 
 static void soft_vm_show_registers(struct soft_vm * vm)
