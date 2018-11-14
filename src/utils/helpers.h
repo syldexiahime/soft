7826fc9083c93541964142df239f877116162b37
Sophie Askew <sophie@ofthewi.red>
2018-11-14 20:40:32 +0000

    rewrote vm to use nanboxes

--- src/utils/helpers.h
+++ src/utils/helpers.h
@@ -15,11 +15,19 @@ int   parse_int(char * s);
 int   parse_hex(char * s);
 int   parse_oct(char * s);
 float parse_float(char * s);
+float parse_double(char * s);
+
+char * int_to_str(int i);
+char * float_to_str(float f);
+char * double_to_str(double f);
 
 bool ishex(char c);
 bool isoct(char c);
 bool isbin(char c);
 
 char * str_replace_at(char * dst, char * src, size_t pos, size_t len);
+
+// temporary definiton, replace with actual implementation later
+#define smalloc(size) malloc(size)
 
 #endif // _SOFT_HELPERS_H
--- tests/soft-test.c
+++ tests/soft-test.c
@@ -2,65 +2,9 @@
 
 #define SOFT_VM_TESTS 1
 #define SOFT_UTILS_TESTS 1
-#define SOFT_ASM_TOKENIZER_TESTS 1
-
-#define REGISTER_VM_UNIT_TEST_SUITE(instr) \
-	cmocka_unit_test(soft_vm_test_##instr##_int32_reg), \
-	cmocka_unit_test(soft_vm_test_##instr##_int32_imm), \
-	cmocka_unit_test(soft_vm_test_##instr##_float_reg), \
-	cmocka_unit_test(soft_vm_test_##instr##_float_imm)
 
 int main(void) {
-    const struct CMUnitTest tests[] = {
-
-		/**
-		 * Virtual machine tests
-		 * */
-		#ifdef SOFT_VM_TESTS
-        cmocka_unit_test(soft_vm_test_load_ptr),
-        cmocka_unit_test(soft_vm_test_load_int32),
-        cmocka_unit_test(soft_vm_test_load_float),
-
-		cmocka_unit_test(soft_vm_test_mov_reg_int32_to_reg),
-		cmocka_unit_test(soft_vm_test_mov_reg_int32_to_addr),
-		cmocka_unit_test(soft_vm_test_mov_reg_float_to_reg),
-		cmocka_unit_test(soft_vm_test_mov_reg_float_to_addr),
-		cmocka_unit_test(soft_vm_test_mov_addr_int32_to_reg),
-		cmocka_unit_test(soft_vm_test_mov_addr_float_to_reg),
-		cmocka_unit_test(soft_vm_test_mov_addr_int32_to_addr),
-		cmocka_unit_test(soft_vm_test_mov_addr_float_to_addr),
-
-		REGISTER_VM_UNIT_TEST_SUITE(add),
-		REGISTER_VM_UNIT_TEST_SUITE(sub),
-		REGISTER_VM_UNIT_TEST_SUITE(mul),
-		REGISTER_VM_UNIT_TEST_SUITE(div),
-		cmocka_unit_test(soft_vm_test_add_ptr_reg),
-		cmocka_unit_test(soft_vm_test_add_ptr_imm),
-		cmocka_unit_test(soft_vm_test_sub_ptr_reg),
-		cmocka_unit_test(soft_vm_test_sub_ptr_imm),
-
-		cmocka_unit_test(soft_vm_test_and_int32_reg),
-		cmocka_unit_test(soft_vm_test_or_int32_reg),
-		cmocka_unit_test(soft_vm_test_xor_int32_reg),
-		cmocka_unit_test(soft_vm_test_lshift_int32_reg),
-		cmocka_unit_test(soft_vm_test_rshift_int32_reg),
-
-		cmocka_unit_test(soft_vm_test_and_int32_imm),
-		cmocka_unit_test(soft_vm_test_or_int32_imm),
-		cmocka_unit_test(soft_vm_test_xor_int32_imm),
-		cmocka_unit_test(soft_vm_test_lshift_int32_imm),
-		cmocka_unit_test(soft_vm_test_rshift_int32_imm),
-
-		REGISTER_VM_UNIT_TEST_SUITE(eq),
-		REGISTER_VM_UNIT_TEST_SUITE(gt),
-		REGISTER_VM_UNIT_TEST_SUITE(lt),
-		REGISTER_VM_UNIT_TEST_SUITE(gteq),
-		REGISTER_VM_UNIT_TEST_SUITE(lteq),
-
-		cmocka_unit_test(soft_vm_test_jmp),
-		cmocka_unit_test(soft_vm_test_jmpz),
-		cmocka_unit_test(soft_vm_test_jmpnz),
-		#endif
+	const struct CMUnitTest tests[] = {
 
 		#ifdef SOFT_UTILS_TESTS
 		cmocka_unit_test(soft_charstream_test_peek),
@@ -72,6 +16,15 @@ int main(void) {
 
 		cmocka_unit_test(soft_test_str_replace_at),
 
+		cmocka_unit_test(soft_test_numbers_to_str),
+
+		cmocka_unit_test(soft_test_tagged_pointers),
+		#endif
+
+		/**
+		 * Virtual machine tests
+		 * */
+		#ifdef SOFT_VM_TESTS
 		cmocka_unit_test(soft_test_sval_null),
 		cmocka_unit_test(soft_test_sval_true),
 		cmocka_unit_test(soft_test_sval_false),
@@ -82,7 +35,11 @@ int main(void) {
 		cmocka_unit_test(soft_test_sval_string),
 		cmocka_unit_test(soft_test_sval_falsey),
 
-		cmocka_unit_test(soft_test_tagged_pointers),
+		cmocka_unit_test(soft_vm_test_dloadi),
+		cmocka_unit_test(soft_vm_test_dload),
+		cmocka_unit_test(soft_vm_test_dstore),
+		cmocka_unit_test(soft_vm_test_dmovi),
+		cmocka_unit_test(soft_vm_test_dmov),
 		#endif
 
 		#ifdef SOFT_ASM_TOKENIZER_TESTS
