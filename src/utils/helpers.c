17f2f8de4f78715ad3ad65329bec3ed571498d9d
Syldexia <syldexia@ofthewi.red>
2018-11-15 00:23:30 +0000

    wrote dynamic arithmetic instructions

--- src/utils/helpers.c
+++ src/utils/helpers.c
@@ -1,10 +1,10 @@
 #include "utils/helpers.h"
 
-inline int   parse_int(char * s)    {    int n; sscanf(s, "%d", &n); return n; }
-inline int   parse_hex(char * s)    {    int n; sscanf(s, "%x", &n); return n; }
-inline int   parse_oct(char * s)    {    int n; sscanf(s, "%o", &n); return n; }
-inline float parse_float(char * s)  {  float n; sscanf(s, "%f", &n); return n; }
-inline float parse_double(char * s) { double n; sscanf(s, "%f", &n); return n; }
+inline int    parse_int(char * s)    {    int n; sscanf(s, "%d",  &n); return n; }
+inline int    parse_hex(char * s)    {    int n; sscanf(s, "%x",  &n); return n; }
+inline int    parse_oct(char * s)    {    int n; sscanf(s, "%o",  &n); return n; }
+inline float  parse_float(char * s)  {  float n; sscanf(s, "%f",  &n); return n; }
+inline double parse_double(char * s) { double n; sscanf(s, "%lf", &n); return n; }
 
 inline char * int_to_str(int i)       { char * b = smalloc(snprintf(NULL, 0, "%d", i)); sprintf(b, "%d", i);     return b; }
 inline char * float_to_str(float f)   { char * b = smalloc(snprintf(NULL, 0, "%f", f)); sprintf(b, "%f", f); return b; }
@@ -13,6 +13,16 @@ inline char * double_to_str(double d) { char * b = smalloc(snprintf(NULL, 0, "%f
 inline bool ishex(char c) { return c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e' || c == 'f' || isnumber(c); }
 inline bool isoct(char c) { return c != '8' && c != '9' && isnumber(c); }
 inline bool isbin(char c) { return c == '0' || c == '1'; }
+
+bool str_is_double(char * str)
+{
+	for (size_t i = 0; str[i] != '\0'; i++) {
+		if (str[i] == '.')    return true;
+		if (!isnumber(str[i])) return false;
+	}
+
+	return false;
+}
 
 char * str_replace_at(char * dst, char * src, size_t pos, size_t rlen)
 {
