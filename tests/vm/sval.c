c55825a68a06732776b182f393dd135eda1bd4e0
Sophie Askew <sophie@ofthewi.red>
2018-11-17 20:06:19 +0000

    moved and wrote unit tests for sval arithmetic

--- tests/vm/sval.c
+++ tests/vm/sval.c
@@ -252,3 +252,103 @@ void soft_test_sval_casts(void ** state)
 	assert_true(sval_cast_to_int(v) == 23);
 	assert_true(sval_cast_to_double(v) == (double) 23.45);
 }
+
+void soft_test_sval_arithmetic_int(void ** state)
+{
+	sval_t v;
+
+	sval_t i      = sval_from_int(17);
+	sval_t d      = sval_from_double(12.3);
+	sval_t istr   = sval_from_string(smalloc(sizeof(char) * 2));
+	sval_t dstr   = sval_from_string(smalloc(sizeof(char) * 5));
+	sval_t nanstr = sval_from_string(smalloc(sizeof(char) * 6));
+
+	strcpy(sval_to_string(istr), "73");
+	strcpy(sval_to_string(dstr), "54.32");
+	strcpy(sval_to_string(nanstr), "1.5NaN");
+
+	/* dynamic integer arithmetic */
+	sval_arithmetic(v, i, +, i);
+	assert_true(sval_to_int(v) == 34);
+	sval_arithmetic(v, i, +, d);
+	assert_true(sval_to_double(v) == 29.3);
+	sval_arithmetic(v, i, +, istr);
+	assert_true(sval_to_int(v) == 90);
+	sval_arithmetic(v, i, +, dstr);
+	assert_true(sval_to_double(v) == 71.32);
+	sval_arithmetic(v, i, +, sval_nan());
+	assert_true(sval_is_nan(v));
+	sval_arithmetic(v, i, +, nanstr);
+	assert_true(sval_is_nan(v));
+}
+
+void soft_test_sval_arithmetic_double(void ** state)
+{
+	sval_t v;
+
+	sval_t i      = sval_from_int(17);
+	sval_t d      = sval_from_double(12.3);
+	sval_t istr   = sval_from_string(smalloc(sizeof(char) * 2));
+	sval_t dstr   = sval_from_string(smalloc(sizeof(char) * 5));
+	sval_t nanstr = sval_from_string(smalloc(sizeof(char) * 6));
+
+	strcpy(sval_to_string(istr), "73");
+	strcpy(sval_to_string(dstr), "54.32");
+	strcpy(sval_to_string(nanstr), "1.5NaN");
+
+	/* dynamic integer arithmetic */
+	sval_arithmetic(v, d, +, i);
+	assert_true(sval_to_double(v) == 29.3);
+	sval_arithmetic(v, d, +, d);
+	assert_true(sval_to_double(v) == 24.6);
+	sval_arithmetic(v, d, +, istr);
+	assert_true(sval_to_double(v) == 85.3);
+	sval_arithmetic(v, d, +, dstr);
+	assert_true(sval_to_double(v) == 66.62);
+	sval_arithmetic(v, d, +, sval_nan());
+	assert_true(sval_is_nan(v));
+	sval_arithmetic(v, d, +, nanstr);
+	assert_true(sval_is_nan(v));
+}
+
+void soft_test_sval_arithmetic_string(void ** state)
+{
+	sval_t v;
+
+	sval_t i      = sval_from_int(17);
+	sval_t d      = sval_from_double(12.3);
+	sval_t istr   = sval_from_string(smalloc(sizeof(char) * 2));
+	sval_t dstr   = sval_from_string(smalloc(sizeof(char) * 5));
+	sval_t nanstr = sval_from_string(smalloc(sizeof(char) * 6));
+
+	strcpy(sval_to_string(istr), "73");
+	strcpy(sval_to_string(dstr), "54.32");
+	strcpy(sval_to_string(nanstr), "1.5NaN");
+
+	sval_arithmetic(v, istr, +, i);
+	assert_true(sval_to_int(v) == 90);
+	sval_arithmetic(v, istr, +, d);
+	assert_true(sval_to_double(v) == 85.3);
+	sval_arithmetic(v, istr, +, istr);
+	assert_true(sval_to_int(v) == 146);
+	sval_arithmetic(v, istr, +, dstr);
+	assert_true(sval_to_double(v) == 127.32);
+	sval_arithmetic(v, istr, +, sval_nan());
+	assert_true(sval_is_nan(v));
+	sval_arithmetic(v, istr, +, nanstr);
+	assert_true(sval_is_nan(v));
+
+	sval_arithmetic(v, dstr, +, i);
+	assert_true(sval_to_double(v) == 71.32);
+	sval_arithmetic(v, dstr, +, d);
+	assert_true(sval_to_double(v) == 66.62);
+	sval_arithmetic(v, dstr, +, istr);
+	assert_true(sval_to_double(v) == 127.32);
+	sval_arithmetic(v, dstr, +, dstr);
+	assert_true(sval_to_double(v) == 108.64);
+	sval_arithmetic(v, dstr, +, sval_nan());
+	assert_true(sval_is_nan(v));
+	sval_arithmetic(v, dstr, +, nanstr);
+	assert_true(sval_is_nan(v));
+}
+
