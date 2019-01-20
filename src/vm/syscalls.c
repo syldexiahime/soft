dd7cc42eb94a912db5a2206e7620defefa7b8e9b
Syldexia <syldexia@ofthewi.red>
2019-01-20 00:08:08 +0000

    rewrote vm to use word semantics for registers

--- src/vm/syscalls.c
+++ src/vm/syscalls.c
@@ -1,4 +1,5 @@
 #include "vm/syscalls.h"
+#include "utils/helpers.h"
 
 
 void soft_vm_syscall(struct soft_vm * vm, struct soft_instr instr)
@@ -6,41 +7,41 @@ void soft_vm_syscall(struct soft_vm * vm, struct soft_instr instr)
 	switch (instr.imm) {
 
 		case soft_sys_open: {
-			char*  buffer = (char *)  sval_to_pointer(vm->r[0]);
-			int    oflags =          vm->r[1].as_int;
-			mode_t mode   = (mode_t) vm->r[2].as_int;
+			char*  buffer = (char *) vm->r[0].dw;
+			int    oflags = (int)    vm->r[1].w;
+			mode_t mode   = (mode_t) vm->r[2].w;
 
-			vm->r[instr.dst].as_int = open(buffer, oflags, mode);
+			vm->r[instr.dst].w = bitwise_cast(word_t, ssize_t, open(buffer, oflags, mode));
 			break;
 		}
 
 		case soft_sys_read: {
-			int     fildes =          vm->r[0].as_int;
-			void *  buffer =          sval_to_pointer(vm->r[1]);
-			size_t  nbytes = (size_t) vm->r[2].as_int;
+			int     fildes = (int)    vm->r[0].w;
+			void *  buffer = (void *) vm->r[1].dw;
+			size_t  nbytes = (size_t) vm->r[2].w;
 
-			vm->r[instr.dst].as_int = read(fildes, buffer, nbytes);
+			vm->r[instr.dst].w = bitwise_cast(word_t, ssize_t, read(fildes, buffer, nbytes));
 			break;
 		}
 
 		case soft_sys_write: {
-			int     fildes =          vm->r[0].as_int;
-			void *  buffer =          sval_to_pointer(vm->r[1]);
-			size_t  nbytes = (size_t) vm->r[2].as_int;
+			int     fildes = (int)    vm->r[0].w;
+			void *  buffer = (void *) vm->r[1].dw;
+			size_t  nbytes = (size_t) vm->r[2].w;
 
-			vm->r[instr.dst].as_int = write(fildes, buffer, nbytes);
+			vm->r[instr.dst].w = bitwise_cast(word_t, ssize_t, write(fildes, buffer, nbytes));
 			break;
 		}
 
 		case soft_sys_malloc: {
-			size_t nbytes = (size_t) vm->r[0].as_int;
+			size_t nbytes = (size_t) vm->r[0].w;
 
 			// vm->r[instr.dst] = sval_from_pointer(malloc(nbytes));
 			break;
 		}
 
 		case soft_sys_free: {
-			void * ptr = sval_to_pointer(vm->r[0]);
+			void * ptr = (void *) vm->r[0].dw;
 
 			free(ptr);
 			break;
