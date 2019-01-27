039b94fb164b26d22a8e1ba0749af5e3b9c68b4e
Syldexia <syldexia@ofthewi.red>
2019-01-27 01:03:18 +0000

    added jump instructions

--- tests/vm/load.c
+++ tests/vm/load.c
@@ -4,10 +4,12 @@ void soft_vm_test_load_dw(void ** state)
 {
 	sval_t datastore[] = {
 		sval_from_int(17),
+		sval_from_double(17.17),
 	};
 
 	struct soft_instr instructions[] = {
 		sinstr(load_dw, 0, soft_rax, 0),
+		sinstr(load_dw, 0, soft_rbx, 1 * sizeof(sval_t)),
 		sinstr(halt, 0, 0, 0),
 	};
 
@@ -20,6 +22,7 @@ void soft_vm_test_load_dw(void ** state)
 	soft_vm_run_vm(&vm);
 
 	assert_true(sval_to_int(vm.r[soft_rax].sval) == 17);
+	assert_true(sval_to_double(vm.r[soft_rbx].sval) == 17.17);
 }
 
 void soft_vm_test_loadi(void ** state)
