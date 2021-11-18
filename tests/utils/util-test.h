50ca282eac8803aaf0b1ae95ca7bd2dc65d9f183
Sophie Askew <sophie@ofthewi.red>
2021-11-18 00:08:53 +0000

    wrote simple hash table

--- tests/utils/util-test.h
+++ tests/utils/util-test.h
@@ -10,20 +10,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-void soft_test_str_replace_at(void **state);
+void soft_test_str_replace_at (void **state);
 
-void soft_test_parse_numbers(void ** state);
-void soft_test_numbers_to_str(void ** state);
+void soft_test_parse_numbers (void ** state);
+void soft_test_numbers_to_str (void ** state);
 
-void soft_test_str_is_number(void ** state);
+void soft_test_str_is_number (void ** state);
 
-void soft_test_tagged_pointers(void ** state);
+void soft_test_tagged_pointers (void ** state);
 
-void soft_test_charstream_peek(void ** sate);
-void soft_test_charstream_consume(void ** sate);
-void soft_test_charstream_eof(void ** sate);
-void soft_test_charstream_expect(void ** sate);
-void soft_test_charstream_skip(void ** sate);
-void soft_test_charstream_read_while(void ** sate);
+void soft_test_charstream_peek (void ** sate);
+void soft_test_charstream_consume (void ** sate);
+void soft_test_charstream_eof (void ** sate);
+void soft_test_charstream_expect (void ** sate);
+void soft_test_charstream_skip (void ** sate);
+void soft_test_charstream_read_while (void ** sate);
+
+void soft_test_hashtable (void ** state);
+void soft_test_hashtable_collision (void ** state);
+void soft_test_hashtable_capacity (void ** state);
 
 #endif // _SOFT_UTILS_TEST_H
